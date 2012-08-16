a43aac5ce398bbb3609c4f8a1f3c935fcacee121
Matt Forbes <matt@freshplum.com>
2012-08-16 16:39:45 -0700

    ovrundr stuff

--- static/js/ovrundr.js
+++ static/js/ovrundr.js
@@ -1,4 +1,4 @@
-var id="110325092";
+var _id="110325092";
 
 function ajax(url, callback) {
     var xhr = new XMLHttpRequest();
@@ -49,36 +49,4 @@ function check_id(username) {
     });
 }
 
-function get_and_update() {
-    var s = $('<script>').attr('type', 'text/json').attr('src', 'http://runkeeper.com/ajax/pointData?activityId=110325092&_=1345151546825');
-    $('head').append(s);
-    (function() {
-        window.s = s;
-        if (s.text().length) {
-            console.log(s.html());
-        } else {
-            //setTimeout(arguments.callee, 500);
-        }
-    })();
-/*
-    console.log(s);
-        resp = JSON.parse(s.innerHtml);
-        var points = resp.points,
-        max = null;
-
-        for (var i in points) {
-            if (!max || points[i].deltaTime > max.deltaTime) {
-                max = points[i];
-            }
-        }
-
-        update(max.latitude, max.longitude);
-*/
-}
-
-$(document).ready(function() {
-    (function() {
-        get_and_update();
-        //setTimeout(arguments.callee, 5000);
-    })();
-});
+update_position(_id);
