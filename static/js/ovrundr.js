0f57372522eca1fa86a157523f9756e8c5fb848c
Matt Forbes <matt@freshplum.com>
2012-08-16 16:49:40 -0700

    ovrundr stuff

--- static/js/ovrundr.js
+++ static/js/ovrundr.js
@@ -1,3 +1,14 @@
+var user = window.location.search.substring(1) || 'NickAlexander'
+  , map_container = document.getElementById('map')
+  , map = new google.maps.Map(map_container, {
+        zoom: 20,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    })
+  , marker = new google.maps.Marker({
+        map: map,
+        draggable: false
+    });
+
 var _id="110325092";
 
 function ajax(url, callback) {
@@ -13,23 +24,8 @@ function ajax(url, callback) {
 
 function update_map(lat, lng) {
     var point = new google.maps.LatLng(lat, lng);
-    var map_container = document.getElementById('map');
-    var options = {
-        zoom: 20,
-        center: point,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-        // mapTypeControl: true,
-        // navigationControl: true
-    };
-
-    console.log(map_container, options);
-    var map = new google.maps.Map(map_container, options);
-
-    var marker = new google.maps.Marker({
-        map: map,
-        position: new google.maps.LatLng(lat, lng),
-        draggable: false
-    });
+    marker.setPosition(point);
+    map.setCenter(point);
 }
 
 function update_position(id) {
@@ -39,7 +35,7 @@ function update_position(id) {
     });
 }
 
-function check_id(username) {
+function check_and_update(username) {
     var url = '/ovrundr/live/' + username;
     ajax(url, function(live) {
         var id = live.liveActivityId;
@@ -49,4 +45,7 @@ function check_id(username) {
     });
 }
 
-update_position(_id);
+(function() {
+    check_and_update(user);
+    setTimeout(arguments.callee, 1000);
+})();
